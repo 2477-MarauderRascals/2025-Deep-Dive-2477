aa0f4c6ead4b7a0b4cfef9dbb5a24474f85c673b
2477-MarauderRascals <ross.sabido26@gmail.com>
2025-03-31 15:23:30 -1000

    2025 Finished Product

---
+++ src/main/cpp/Robot.cpp
@@ -0,0 +1,102 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+#include "Robot.h"
+
+#include <frc/DriverStation.h>
+#include <frc/PS5Controller.h>
+#include <frc/XboxController.h>
+#include <frc/smartdashboard/SmartDashboard.h>
+#include <frc2/command/CommandScheduler.h>
+#include <wpi/print.h>
+
+
+void Robot::RobotInit() {
+    frc::CameraServer::StartAutomaticCapture();
+
+}
+
+/**
+ * This function is called every 20 ms, no matter the mode. Use
+ * this for items like diagnostics that you want to run during disabled,
+ * autonomous, teleoperated and test.
+ *
+ * <p> This runs after the mode specific periodic functions, but before
+ * LiveWindow and SmartDashboard integrated updating.
+ */
+void Robot::RobotPeriodic() { frc2::CommandScheduler::GetInstance().Run(); }
+
+/**
+ * This function is called once each time the robot enters Disabled mode. You
+ * can use it to reset any subsystem information you want to clear when the
+ * robot is disabled.
+ */
+void Robot::DisabledInit() {}
+
+
+void Robot::DisabledPeriodic() {}
+
+/**
+ * This autonomous runs the autonomous command selected by your {@link
+ * RobotContainer} class.
+ */
+void Robot::AutonomousInit() {
+  m_autonomousCommand = m_container.GetAutonomousCommand();
+
+  if (m_autonomousCommand != nullptr) {
+    m_autonomousCommand->Schedule();
+  }
+}
+
+void Robot::AutonomousPeriodic() {
+  
+}
+
+void Robot::TeleopInit() {
+  // This makes sure that the autonomous stops running when
+  // teleop starts running. If you want the autonomous to
+  // continue until interrupted by another command, remove
+  // this line or comment it out.
+  if (m_autonomousCommand != nullptr) {
+    m_autonomousCommand->Cancel();
+    m_autonomousCommand = nullptr;
+  }
+}
+
+/**
+ * This function is called periodically during operator control.
+ */
+void Robot::TeleopPeriodic() {
+  bool AButton = Xbox.GetRawButton(1);
+  bool BButton = Xbox.GetRawButton(2);
+  bool XButton = Xbox.GetRawButton(3);
+  bool YButton = Xbox.GetRawButton(4);
+  bool LBumper = Xbox.GetRawButton(5);
+  bool RBumper = Xbox.GetRawButton(6);
+  bool LTrigger = Xbox.GetRawAxis(2);
+  bool RTrigger = Xbox.GetRawAxis(4);
+
+  Algae.CheckInput();
+  ClawHand.CheckInput();
+  Elevator.CheckInput();
+  
+  frc::XboxController xboxController{0};  // Controller on port 0
+                                          // Read the Xbox controller buttons
+
+  frc::PS5Controller PS5C{1};
+  double Speed = .50;
+
+
+
+ 
+}
+
+/**
+ * This function is called periodically during test mode.
+ */
+void Robot::TestPeriodic() {}
+
+#ifndef RUNNING_FRC_TESTS
+int main() { return frc::StartRobot<Robot>(); }
+#endif
---
+++ src/main/include/Robot.h
@@ -0,0 +1,44 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+#pragma once
+
+#include <frc/TimedRobot.h>
+#include <frc2/command/Command.h>
+#include <frc/Encoder.h>
+#include "RobotContainer.h"
+
+#include "subsystems/Claw.h"
+#include "subsystems/AlgaeInt.h"
+#include "subsystems/Vertical.h"
+
+#include <cameraserver/CameraServer.h>
+
+class Robot : public frc::TimedRobot {
+ public:
+  void RobotInit() override;
+  void RobotPeriodic() override;
+  void DisabledInit() override;
+  void DisabledPeriodic() override;
+  void AutonomousInit() override;
+  void AutonomousPeriodic() override;
+  void TeleopInit() override;
+  void TeleopPeriodic() override;
+  void TestPeriodic() override;
+AlgaeInt Algae;
+  Claw ClawHand;
+  Vertical Elevator;
+  frc::Encoder testEncoder{0, 1, false, frc::Encoder::EncodingType::k2X};
+  frc::XboxController Xbox{0};
+
+ private:
+  // Have it null by default so that if testing teleop it
+  // doesn't have undefined behavior and potentially crash.
+  frc2::Command* m_autonomousCommand = nullptr;
+  frc::SendableChooser<std::string> m_chooser;
+  const std::string kAutoNameDefault = "Default";
+  const std::string kAutoNameCustom = "My Auto";
+  std::string m_autoSelected;
+  RobotContainer m_container;
+};
